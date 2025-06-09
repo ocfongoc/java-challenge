bcba131292fb4cf14b886a5322c5c375753ea18c
Bosco Fong <boscofong@Boscos-MacBook-Air.local>
2025-06-09 11:36:38 +0800

    add cache

--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -3,13 +3,15 @@ package jp.co.axa.apidemo.services;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
+public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -18,24 +20,30 @@ public class EmployeeServiceImpl implements EmployeeService{
         this.employeeRepository = employeeRepository;
     }
 
+    @Cacheable(value = "employees")
     public List<Employee> retrieveEmployees() {
         List<Employee> employees = employeeRepository.findAll();
         return employees;
     }
 
+    @Cacheable(value = "employee", key = "#employeeId")
     public Employee getEmployee(Long employeeId) {
-        Optional<Employee> optEmp = employeeRepository.findById(employeeId);
-        return optEmp.get();
+        return employeeRepository.findById(employeeId).orElse(null);
     }
 
-    public void saveEmployee(Employee employee){
+    @CacheEvict(value = "employees", allEntries = true)
+    @CachePut(value = "employee", key = "#employee.id")
+    public void saveEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
 
-    public void deleteEmployee(Long employeeId){
+    @CacheEvict(value = {"employees", "employee"}, allEntries = true)
+    public void deleteEmployee(Long employeeId) {
         employeeRepository.deleteById(employeeId);
     }
 
+    @CacheEvict(value = "employees", allEntries = true)
+    @CachePut(value = "employee", key = "#employee.id")
     public void updateEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
